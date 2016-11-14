62dd96e8cf188dd9d8657e82f2b221f1862f6dae
kvch555 <185944345@qq.com>
2016-11-14 15:36:19 +0800

    笔记整理

---
+++ Ajax彻底研究/04.Ajax返回值之XML类/08-returntype.php
@@ -0,0 +1,15 @@
+<?php
+/****
+自学it网 高端PHP培训
+
+论  坛: http://www.zixue.it
+微  博: http://weibo.com/Yshiba
+****/
+header('Content-Type: text/xml');
+?>
+<?xml version="1.0" encoding="utf-8"?>
+<bookstore><book bid="b008"><title>天龙八部</title><intro>人生太苦了</intro></book></bookstore>
+
+
+
+
