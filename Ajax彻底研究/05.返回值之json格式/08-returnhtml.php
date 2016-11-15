ac602f73a05f97f661fa9b4a9eb2e55e5fe14ce2
kvch555 <185944345@qq.com>
2016-11-15 17:57:46 +0800

    笔记整理

---
+++ Ajax彻底研究/05.返回值之json格式/08-returnhtml.php
@@ -0,0 +1,20 @@
+<?php
+/****
+自学it网 高端PHP培训
+
+论  坛: http://www.zixue.it
+微  博: http://weibo.com/Yshiba
+****/
+
+/*
+从数据库取出N条数据
+*/
+
+foreach(array('新闻1','新闻2','新闻3') as $v) {
+    echo '<li>',$v,'</li>';
+}
+?>
+
+
+
+
---
+++ Ajax彻底研究/05.返回值之json格式/08-returnjson.php
@@ -0,0 +1,19 @@
+<?php
+/****
+自学it网 高端PHP培训
+
+论  坛: http://www.zixue.it
+微  博: http://weibo.com/Yshiba
+****/
+
+// JavaScriptObjectNotation
+
+/*
+{title:'天龙八部',intro:'人生八苦'}
+*/
+$book = array('title'=>'天龙八部','intro'=>'人生八苦');
+echo json_encode($book);
+
+// json_decode
+
+?>
